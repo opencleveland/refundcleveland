841b300b5094b32ebbdf56ec1a658fec85f9c804
Jessica Lynch <jessica.lynch@trojans.dsu.edu>
2020-10-01 08:20:43 -0400

    Replace .env with local_settings.py

---
+++ local_settings_template.py
@@ -0,0 +1,3 @@
+# rename to local_settings.py
+DEBUG="TRUE"
+SECRET_KEY="ENTER KEY HERE" # Django secret key
--- refund_cleveland/settings.py
+++ refund_cleveland/settings.py
@@ -12,12 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import dotenv
-
-dotenv.load_dotenv(
-    os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
-)
-
+from local_settings import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,12 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
