eef02b3c869a45653831544196fbc512ed0d43ff
Jessica Lynch <jessica.lynch@trojans.dsu.edu>
2020-10-01 11:30:09 -0400

    Add active class to current nav link

--- peoples_budget/views.py
+++ peoples_budget/views.py
@@ -3,13 +3,24 @@ from django.shortcuts import HttpResponse
 
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {
+        'home': True,
+    })
+
 
 def budget(request):
-    return render(request, 'budget.html')
+    return render(request, 'budget.html', {
+        'budget': True,
+    })
+
 
 def join(request):
-    return render(request, 'join.html')
+    return render(request, 'join.html', {
+        'join': True,
+    })
+
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'about.html', {
+        'about': True,
+    })
