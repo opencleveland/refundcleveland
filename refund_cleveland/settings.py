01c2c416fa54aee973da9c5718bed22f019ce3b0
Liz <shuxrei@gmail.com>
2020-12-19 09:32:53 -0500

    Converting database path to string for compatibility

--- refund_cleveland/settings.py
+++ refund_cleveland/settings.py
@@ -72,7 +72,7 @@ WSGI_APPLICATION = 'refund_cleveland.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
