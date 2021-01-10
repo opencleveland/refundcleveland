c2ccf97d540490dc0f21559ea1b009531456025e
Jessica Lynch <jessica.lynch@trojans.dsu.edu>
2021-01-09 20:51:04 -0500

    Renamed JS files for clarity

--- refund_cleveland/settings.py
+++ refund_cleveland/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 from local_settings import *
-# import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
