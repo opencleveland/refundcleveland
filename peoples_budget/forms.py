6842cb108697ced52c6b0b7f8e30a1fe2c4fd4b6
Adam <adamfishman@gmail.com>
2021-02-07 12:43:50 -0500

    restrict ward to integer range of 1-17

--- peoples_budget/forms.py
+++ peoples_budget/forms.py
@@ -1,5 +1,13 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
+def validate_ward(value):
+    if value < 1 or value > 17:
+        raise ValidationError(
+            _('%(value)s is not a valid ward (1-17)'),
+            params={'value': value},
+        )
 
 class ChangeBudgetForm(forms.Form):
     """Hidden form to store user budget data"""
@@ -8,5 +16,5 @@ class ChangeBudgetForm(forms.Form):
 class SubmitBudgetForm(forms.Form):
     """Combine user budget data and PII"""
     email = forms.EmailField(required=True)
-    ward = forms.CharField(required=True)
+    ward = forms.IntegerField(required=True, validators=[validate_ward])
     json_data = forms.CharField(widget=forms.HiddenInput(), required=True)
